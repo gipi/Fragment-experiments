6d1b326151fa72d41471f529fe4ce416e24eebf9
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2011-10-23 12:33:13 +0200

    Show some content in the list.

--- src/org/ktln2/android/FragmentExperimentsActivity.java
+++ src/org/ktln2/android/FragmentExperimentsActivity.java
@@ -3,6 +3,8 @@ package org.ktln2.android;
 import android.app.Activity;
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
 
 public class FragmentExperimentsActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -23,6 +25,20 @@ public class FragmentExperimentsActivity extends Activity {
 	 * The aim of this class is to show the items.
 	 */
 	static public class ItemsFragment extends ListFragment {
+		@Override
+		public void onActivityCreated(Bundle b) {
+			super.onActivityCreated(b);
+
+			setListAdapter(
+				new ArrayAdapter<String>(getActivity(),
+					android.R.layout.simple_list_item_activated_1,
+					new String[] {
+						"miao",
+						"bau",
+					}
+				)
+			);
+		}
 	}
 
 }
